7a928fff3c813a8af11b766325e4db0333e27c58
Sorunome <mail@sorunome.de>
2019-08-14 17:47:43 +0200

    forgot docekr-run.sh

---
+++ docker-run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+if [ ! -f "/data/config.yaml" ]; then
+	echo "No config found"
+	exit 1
+fi
+if [ ! -f "/data/instagram-registration.yaml" ]; then
+	node /opt/mx-puppet-instagram/build/index.js -r
+	echo "Registration generated."
+	exit 0
+fi
+node /opt/mx-puppet-instagram/build/index.js -c /data/config.yaml -f /data/instagram-registration.yaml
